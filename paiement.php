a82abd4a087eeb5809344e9e5907eeaf4f098b23
Benjamin Gauthier <benjamin-gauthier@live.fr>
2015-03-11 21:33:26 +0100

    Ajout de la page paiement

---
+++ paiement.php
@@ -0,0 +1,29 @@
+<?php include ('header.php'); ?>
+<section class="paiement" >
+    <section class="paiement-header">
+        <div class="jumbotron speaker-jumbotron">
+            <h2>Félicitations ! Vous avez été tiré au sort pour assister au premier <span class="red">TEDxUniversitéDeBordeaux</span> !</h2>
+            <p>
+
+            </p>
+        </div>
+    </section><!-- simple -->
+    <div class="wrap">
+        <div class="row">
+            <div class="col-md-8">
+                <script type="text/javascript" src="https://www.yuticket.com/media/js/iframe.js"></script>
+                <iframe id="45049211-6f54-4ffd-8b67-b0eba1455ba5" class="noAutoResize" src="https://www.yuticket.com/iframe/ticketing/45049211-6f54-4ffd-8b67-b0eba1455ba5?color=e62b1e" allowTransparency="true" frameBorder="0" style="border: 0" width="600" height="400">Votre navigateur ne peut pas afficher ce contenu. La billetterie est aussi disponible sur <a href="https://www.yuticket.com/tedxuniversitedebordeaux/45049211-6f54-4ffd-8b67-b0eba1455ba5-tedxuniversitedebordeaux.html">Yuticket.com</a>.</iframe>
+            </div>
+            <div class="col-md-4">
+                <p class="paiement-descri">
+                    Il ne reste plus qu’à valider votre inscription en utilisant la plateforme sécurisée ci-contre; les instructions détaillées vous ont été envoyées par email. <br>Vous aurez besoin de créer un compte Yuticket, c’est très rapide et cela n’engage à rien.<br><br>
+
+                    Si vous rencontrez des difficultés, n’hésitez pas à nous contacter : <a href="mailto:tedxuniversitedebordeaux@gmail.com">tedxuniversitedebordeaux@gmail.com</a><br><br>
+
+                    A très bientôt pour une belle journée TEDx !
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include ('footer.php'); ?>
